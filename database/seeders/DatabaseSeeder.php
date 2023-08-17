9c083763ff3ff4cbbf5bef2c57099c7c3260a9b6
SaDKinG <hker0004@gmail.com>
2023-08-17 23:37:55 +0100

    Add seeders to easypanel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +19,60 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Fun', 'position' => 0],
             ['name' => 'Work', 'position' => 0],
             ['name' => 'Daily', 'position' => 0],
+        ]);
+        User::create([
+            'name'=> 'admin',
+            'email'=> 'admin@admin.com',
+            'password'=> 'password',
+            'username'=> 'admin',
+            'user_type'=> 'admin',
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'super_admin',
+            'permissions' => 'a:1:{s:10:"fullAccess";i:1;}',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('panel_admins')->insert([
+            'is_superuser' => true,
+            'user_id' => 1,
+            'updated_at' => now(),
+            'created_at' => now(),
+        ]);
+        DB::table('role_user')->insert([
+            'role_id' => 1,
+            'user_id' => 1,
+        ]);
+        DB::table('cruds')->insert([
+            'name' => 'User',
+            'model' => 'App\Models\User',
+            'route' => 'user',
+            'icon' => 'fa fa-user',
+            'active' => 1,
+            'built' => 1,
+            'with_acl' => 1,
+            'with_policy' => 1,
+        ]);
+        DB::table('cruds')->insert([
+            'name' => 'Task',
+            'model' => 'App\Models\Task',
+            'route' => 'task',
+            'icon' => 'fa fa-tasks',
+            'active' => 1,
+            'built' => 1,
+            'with_acl' => 1,
+            'with_policy' => 1,
+        ]);
+        DB::table('cruds')->insert([
+            'name' => 'Category',
+            'model' => 'App\Models\Category',
+            'route' => 'category',
+            'icon' => 'fa fa-cubes',
+            'active' => 1,
+            'built' => 1,
+            'with_acl' => 1,
+            'with_policy' => 1,
         ]);
     }
 }
